de09484939093e7b8a0fd501d98357b9baf5b009
CoreCreate.io <josh@corecreate.io>
2025-04-29 12:58:14 +1000

    fix: correct params again for influencers

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from 'next'
-import type { Configuration, RuleSetRule } from 'webpack'
+import type { NextConfig } from 'next';
+import type { Configuration, RuleSetRule } from 'webpack';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
   },
   webpack(config: Configuration) {
     const fileLoaderRule = config.module?.rules?.find((rule): rule is RuleSetRule => {
@@ -19,17 +19,17 @@ const nextConfig: NextConfig = {
     });
 
     if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i;
+      fileLoaderRule.exclude = /\.svg$/i; // Exclude SVGs from the default file loader
     }
 
     config.module?.rules?.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: ['@svgr/webpack'], // Use @svgr/webpack for SVGs
     });
 
     return config;
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
